433d567868ff4ee2d953c20f6002a05599bf7f57
Amit Yadav <amit.yadav117019@gmail.com>
2021-12-06 10:06:49 +0530

    final

--- Server/models/postMessage.js
+++ Server/models/postMessage.js
@@ -3,19 +3,18 @@ import mongoose from "mongoose";
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
+    name: String,
     creator: String,
     tags: [String],
-    selectedFile: "",
-    likeCount: {
-        type: Number,
-        default: 0,
-    },
+    selectedFile: String,
+    likes: { type: [String], default: [] },
+    comments: { type: [String], default: [] },
     createdAt: {
         type: Date,
         default: new Date(),
     },
 });
 
-const PostMessage = mongoose.model("postMessage", postSchema);
+var PostMessage = mongoose.model("PostMessage", postSchema);
 
 export default PostMessage;
--- Server/routes/posts.js
+++ Server/routes/posts.js
@@ -1,18 +1,29 @@
 import express from "express";
+
 import {
     getPosts,
+    getPostsBySearch,
+    getPostsByCreator,
+    getPost,
     createPost,
     updatePost,
-    deletePost,
     likePost,
+    commentPost,
+    deletePost,
 } from "../controllers/posts.js";
 
 const router = express.Router();
+import auth from "../middleware/auth.js";
 
+router.get("/creator", getPostsByCreator);
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
-router.post("/", createPost);
-router.patch("/:id", updatePost);
-router.delete("/:id", deletePost);
-router.patch("/:id/LikePost", likePost);
+router.get("/:id", getPost);
+
+router.post("/", auth, createPost);
+router.patch("/:id", auth, updatePost);
+router.delete("/:id", auth, deletePost);
+router.patch("/:id/likePost", auth, likePost);
+router.post("/:id/commentPost", commentPost);
 
 export default router;
